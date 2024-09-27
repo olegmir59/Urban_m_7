dedfbc174c69870690b1500069e221fb9ff69720
olegmir59 <udom1@ya.ru>
2024-09-27 19:58:32 +0700

    Modify File

--- module_7_3.py
+++ module_7_3.py
@@ -11,7 +11,7 @@ class WordsFinder:
         for file_name_ in self.file_names:
             with open(file_name_, mode='r', encoding='utf8') as file:
                 words_str = " ".join(file.readlines()).lower()
-                print(words_str)
+                #print(words_str)
                 new_s = ""
                 for i in range(len(words_str)):
                     if words_str[i] in [',', '.', '=', '!', '?', ';', ':', ' - ', "\n"]:
@@ -19,12 +19,28 @@ class WordsFinder:
                     else:
                         new_s += words_str[i]
                 all_words[file_name_] = new_s.split(" ")
-                file.closed()
+                #file.closed()
 
         return all_words
+
+    def find(self, word):
+        word_dict = self.get_all_words()
+        find_w = str(word.lower())
+        find_res = {}
+        for key, value in word_dict.items():
+            if find_w in value:
+                print(key, ">>>>>",find_w)
+                print(">>> позиция слова в списке: ", value.index(find_w))
+                find_res = {key: value.index(find_w)+1}
+        return find_res
+
+
+
+
+
 
 
 finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words())  # Все слова
-#print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.find('TEXT')) # 3 слово по счёту
 #print(finder2.count('teXT')) # 4 слова teXT в тексте всего
