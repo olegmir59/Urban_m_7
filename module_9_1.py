194dc306bbe49e3a596caf005c6a54c2803b6987
olegmir59 <udom1@ya.ru>
2024-10-03 16:52:03 +0700

    Add file

--- module_9_1.py
+++ module_9_1.py
@@ -3,5 +3,12 @@
 #
 # Задача "Вызов разом":
 
-def apply_all_func(int_list,*functions):
-    
+def apply_all_func(int_list, *functions):
+    result = {}
+    for func_ in functions:
+        result[func_.__name__] = func_(int_list)
+    return result
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
