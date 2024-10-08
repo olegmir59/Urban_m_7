d4a8f561cf6f657e490f383ad37a31e814ffb5dd
olegmir59 <udom1@ya.ru>
2024-10-09 00:08:59 +0700

    Modify file

--- module_10_2.py
+++ module_10_2.py
@@ -31,7 +31,6 @@ first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
 
 first_knight.start()
-#sleep(.5)
 second_knight.start()
 first_knight.join()
 second_knight.join()
