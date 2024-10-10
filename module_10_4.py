b04b873e5e22c14594c8a19d009d0d368f0b0374
olegmir59 <udom1@ya.ru>
2024-10-11 02:16:53 +0700

    Modify file

--- module_10_4.py
+++ module_10_4.py
@@ -8,7 +8,10 @@ from collections import defaultdict
 
 import queue
 import random
-import threading
+from threading import Thread
+
+
+#import threading.Thread
 
 
 class Table:
@@ -17,8 +20,9 @@ class Table:
         self.guest = None
 
 
-class Guest:
+class Guest(Thread):
     def __init__(self,name):
+        super().__init__()
         self.name = name
 
     def run(self):
@@ -30,14 +34,48 @@ class Cafe:
     def __init__(self,*tables):
         self.tables = tables
         self.cafe_queue = queue.Queue(maxsize=20)
-    def guest_arrival(self, *guests):
-        for guest_in in guests:
-            find_a_free_table =False
-            for table in self.tables:
-                if table.guest == None:
-                    table.guest = guest_in
-                    find_a_free_table = True
-                    break
-            if not find_a_free_table:
-                self.cafe_queue.put(guest_in)
 
+    def find_free_tables(self,find_flag):
+        list_free_tables = []
+        list_tables_with_guest =[]
+        for table in self.tables:
+            if table.guest is None:
+                list_free_tables.append(table)
+            else:
+                list_tables_with_guest.append(table)
+        if find_flag:
+            return list_free_tables
+        else:
+            return list_tables_with_guest
+
+    def guest_arrival(self, *guests):
+        list_guests = list(guests)
+        for table in self.find_free_tables(True):
+            if len(list_guests) > 0:
+                table.guest = list_guests.pop(0)
+                print(f"{table.guest.name} сел(-а) за стол номер {table.number}")
+                table.guest.start()         # поток гостя запущен
+        while len(list_guests) > 0:
+                g_ = list_guests.pop(0)
+                self.cafe_queue.put(g_)
+                print(f"{g_.name} в очереди")
+
+
+    def discuss_guests(self):
+        while not self.cafe_queue.empty() and self.find_free_tables(False) is not []:
+            pass
+# Создание столов
+tables = [Table(number) for number in range(1, 6)]
+# Имена гостей
+guests_names = [
+'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+]
+# Создание гостей
+guests = [Guest(name) for name in guests_names]
+# Заполнение кафе столами
+cafe = Cafe(*tables)
+# Приём гостей
+cafe.guest_arrival(*guests)
+# Обслуживание гостей
+#cafe.discuss_guests()
